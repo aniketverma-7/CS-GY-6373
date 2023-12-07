16671ca6e0f91f2e9c53326728f130946a5332b3
Aniket Verma <53976690+aniketverma-7@users.noreply.github.com>
2023-12-07 18:02:07 -0500

    Update q1.pl

--- q1.pl
+++ q1.pl
@@ -1,8 +1,9 @@
 :- initialization(main).
 findMax([X], X).
+findMax([],'List Empty').
 findMax([Head | Tail], Max) :-
     findMax(Tail, TailMax),
     (Head > TailMax -> Max = Head ; Max = TailMax). 
 
 % Example usage:
-main :- findMax([3, 8, 2, 5, 10], Max), write('Max: '), write(Max), nl.
+main :- findMax([1,10,2,3,2,3], Max), write('Result: '), write(Max), nl.
