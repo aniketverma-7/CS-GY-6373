1e4689cde611dd8226da4ee489148eb5b354e3ec
Aniket Verma <53976690+aniketverma-7@users.noreply.github.com>
2023-12-07 18:18:07 -0500

    Update q2.pl

--- q2.pl
+++ q2.pl
@@ -1,12 +1,31 @@
-:- initialization(main).
-hasIntersection([], _) :-fail.
-hasIntersection([H|_], List) :-member(H, List),!.
+hasIntersection([], _) :- fail.
+hasIntersection([H|_], List) :- member(H, List), !.
 hasIntersection([_|T], List) :- hasIntersection(T, List).
 
+checkIntersectionResult([], []) :-
+    write('Both List is empty.'),
+    nl.
+    
+checkIntersectionResult([], List2) :-
+    write('List 1 is empty.'),
+    nl.
+checkIntersectionResult(List1, []) :-
+    write('List 2 is empty.'),
+    nl.
+    
 checkIntersectionResult(List1, List2) :- 
-		(hasIntersection(List1, List2) -> write('Intersection found.');
-																			write('Intersection not found.')).
+    (hasIntersection(List1, List2) -> 
+        write('Intersection found.');
+        write('Intersection not found.')
+    ),
+    nl.
 
-main :- checkIntersectionResult([1,2,3], [1,4,5,6]), nl, 
-				checkIntersectionResult([1,2,3], [4,5,6]), nl, 
-				checkIntersectionResult([], []), nl.
+main :- 
+    checkIntersectionResult([1,2,3], [1,4,5,6]), 
+    checkIntersectionResult([1,2,3], [4,5,6]), 
+    checkIntersectionResult([], []),
+    checkIntersectionResult([], [4,5,6]),
+    checkIntersectionResult([1,2,3], []),
+    nl.
+
+:- initialization(main).
